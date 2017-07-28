8d09838eb44635bb3f6b7f76e8857059d517643e
scvzerng <443683059a>
2017-07-28 21:25:10 +0800

    remove

--- src/main/java/com/zero/scvzerng/Application.java
+++ src/main/java/com/zero/scvzerng/Application.java
@@ -1,8 +1,6 @@
 package com.zero.scvzerng;
 
-import com.zero.scvzerng.entity.CoordBean;
 import com.zero.scvzerng.entity.Coordinate;
-import com.zero.scvzerng.entity.ImageCognition;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -15,14 +13,12 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) throws IOException {
         ImageFinder finder = new ScreenImageFinder();
-        BufferedImage search = ImageIO.read(Application.class.getClassLoader().getResourceAsStream("flow.png"));
+        BufferedImage search = ImageIO.read(Application.class.getClassLoader().getResourceAsStream("help.png"));
         long start = System.currentTimeMillis();
         List<Coordinate> coordinateList = finder.match(search,0.99);
         System.out.println(System.currentTimeMillis()-start);
         start = System.currentTimeMillis();
-        List<CoordBean> coordBeans = new ImageCognition().imageSearch(((ScreenImageFinder)finder).getScreen(),search,ImageCognition.SIM_ACCURATE_VERY);
         System.out.println(System.currentTimeMillis()-start);
         System.out.println(coordinateList.size());
-        System.out.println(coordBeans.size());
     }
 }
